be3990066f407f4c21a890e9ae1b7d0c7bb1640f
Anto8993 <anto8993@stud.kea.dk>
2022-02-24 09:14:09 +0100

    Rettet header js, og comment

--- header.js
+++ header.js
@@ -1,13 +1,16 @@
-const header = document.querySelector("header");
 
-window.onscroll = function() {scrollFunction();};
+
+const header = document.querySelector("header"); //Definerer en konstant til "header"
+
+window.onscroll = function() {scrollFunction();}; //Når der scrolles på vinduet skal funktionen starte
 
 function scrollFunction() {
-    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 20) {
-        header.classList.add("scrolled");
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) { //Hvis der er scrolled mere end 40px ->
+        header.classList.add("scrolled"); //Får headeren classen "scrolled", som giver den en background-color
 
     }
     else {
-        header.classList.toggle("scrolled");
+        header.classList.remove("scrolled"); // Hvis ikke der er scrolled mere end 40px fra toppen fjernes classen
     }
 }
+
